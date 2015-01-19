4459b9f50990d53b88c7e08778996ad134fac321
smhg <sam@strictlyphp.com>
2015-01-19 19:45:26 +0100

    cleanup

--- test/microdata.test.js
+++ test/microdata.test.js
@@ -3,7 +3,6 @@
 'use strict';
 
 var microdata = require('../lib/microdata'),
-  _ = require('underscore'),
   assert = require('assert'),
   fs = require('fs');
 
@@ -31,19 +30,23 @@ describe('microdata', function () {
     describe('with test.html', function () {
       it('should return 2 items and one child', function () {
         var result = microdata.parse(html);
-        assert.equal(result.length, 2);
-        assert.equal(result[0].children.length, 1);
+        assert(result.length === 2);
+        assert(result[0].children.length === 1);
       });
       it('should have a title property on the first child', function () {
         var result = microdata.parse(html);
-        assert(_.has(result[0].itemprop, 'title'));
+        assert('title' in result[0].itemprop);
       });
     });
-    describe('with https://github.com/isaacs/npm', function () {
+    describe('with https://github.com/npm/npm', function () {
       it('should have at least one item', function (done) {
-        var result = microdata.parse('https://github.com/isaacs/npm', function (data) {
-          assert(data.length > 0);
-          done();
+        this.timeout(5000);
+
+        assert.doesNotThrow(function () {
+          microdata.parse('https://github.com/npm/npm', function (data) {
+            assert(data.length > 0);
+            done();
+          });
         });
       });
     });
